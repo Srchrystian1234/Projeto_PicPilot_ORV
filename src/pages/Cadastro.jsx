8780fc7b830614b10e3bd5d35d1b21e00b312c59
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-10-21 11:48:45 -0400

    ola
    Co-Authored-By: Mauricio  Mathias - devoloper <88565329+MauricioMathiass@users.noreply.github.com>

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -82,7 +82,7 @@ export default function Cadastro() {
         const repete = inputRepeatPas.current.value.trim();
         
     
-    
+    //ola mundo//
         nome.replace(/[^A-zÀ-ù\s]/gi,'');
         
         if(!nome){
