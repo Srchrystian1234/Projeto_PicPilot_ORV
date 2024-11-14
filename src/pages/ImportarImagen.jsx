199e5470c4f56c8ba0c9868a001758bb7f4f77ab
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-11-14 16:53:15 -0400

    Criando input de upload de img  pra servidor
    Co-Authored-By: Mauricio  Mathias - devoloper <88565329+MauricioMathiass@users.noreply.github.com>

--- src/pages/ImportarImagen.jsx
+++ src/pages/ImportarImagen.jsx
@@ -1,17 +1,25 @@
 import '../css/ImportarImagen_css/ImportarImagem.css'
 import '../css/Dashbord.css'
 import '../../node_modules/react-datepicker/dist/react-datepicker.css'
-import  { useState } from "react";
+import  { useState,useEffect } from "react";
 
 import MenuLateral from '../components/MenuLateral/MenuLateral'
 export default function ImportarImagen() {
+    //escolha de qual consorcio awa ou social , sao elementos diferentes
+    const [isConsorcio, setisConsorcio] = useState('');
 
-  
+    useEffect (()=>{
+      console.log(isConsorcio)
+    },[isConsorcio])
+
+
+    //seleção de data com valor pro banco 
   const [selectedDate, setSelectedDate] = useState('');
 
   const handleDateChange = (event) => {
     setSelectedDate(event.target.value);
   };
+
   return (
     <div className='cor-de-fundo'>
         <MenuLateral/>
@@ -21,19 +29,19 @@ export default function ImportarImagen() {
                   <div className='orv-info'>
                       <div className='orv-sele-consorcio'>
                         <div className='orv-lado'><label htmlFor="consorcio-select">Selecione um consórcio :</label>
-                            <select  id="consorcio-select">
+                            <select value={isConsorcio} onChange={e => setisConsorcio(e.target.value)} id="consorcio-select">{/*precisa ser passo antes o value pra ter um val*/ }
                             <option value="">Escolha uma opção</option>
                             <option value="awa">Awa</option>
                             <option value="social">Social</option>
                             </select>
                         </div>
-                        <div className='orv-lado'>
-                          <label htmlFor="obra-select">Selecione um Obra :</label>
-                          <select  id="obra-select">
+                        {isConsorcio==='awa'?<><div className='orv-lado'>
+                            <label htmlFor="obra-select">Selecione uma Obra :</label>
+                            <select  id="obra-select">
                             <option value="">Escolha uma opção</option>
                             <option value="obra1">Obra 1</option>
                             <option value="obra2">Obra 2</option>
-                          </select>
+                            </select>
                         </div>
                         <div className='orv-lado'>
                           <label htmlFor="setor-select">Selecione um Setor:</label>
@@ -65,20 +73,61 @@ export default function ImportarImagen() {
                               onChange={handleDateChange}
                             />
                         </div>
+                        </>:
+                        <>
+                        <div className='orv-lado'>
+                        <label htmlFor="data-select">Selecione uma data:</label>
+                            <input
+                              type="date"
+                              id="data-select"
+                              value={selectedDate}
+                              onChange={handleDateChange}
+                            />
+                        </div>
+                        <div className='orv-lado-label'>
+                            <label htmlFor="local-select">Preencha com o título da Ação e Obra</label>
+                            <input type="text" name="local" id="local-select"/>
+                            
+                        </div>
+                        <div className='orv-lado'>
+                            <label htmlFor="periodo-select">Selecione o período:</label>
+                            <select  id="periodo-select">
+                            <option value="">Escolha uma opção</option>
+                            <option value="manha">Manhã</option>
+                            <option value="tarde">tarde</option>
+                            <option value="noite">Noite</option>
+                            </select>
+                        </div>
+                        </>
+                        }
+                        
+                        
+                        
+                        
 
                       </div>
                       <div className='orv-sele-update'>
                         <div className='orv-container-photo'>
                           <div className='orv-update-photo'>
-                              <label className='label-photo' htmlFor="selecionar-photo">Selecionar ou Arrasta fotos..</label>
+                              <label className='label-photo' htmlFor="selecionar-photo">Selecionar ou Arrastar fotos..</label>
                               <input type="file"  id="selecionar-photo"  className='file-input' multiple accept="image/png, image/jpeg"/>
                           </div>
                           <div className='orv-list-photo'>
                             
                               <div className='orv-list-upload'>
-                                  <div className='item'><i className='bi bi-house-fill'></i></div>
-                                  <div className='item'><p>Foto de nome tal...</p></div>
+                                  <div className='item'><i className='icon bi bi-image-fill'></i></div>
+                                  <div className='item'><p> ARQUIVO</p></div>
                                   <div className='item' ><i className='bi bi-check-circle-fill'></i> </div>
+                              </div>
+                              <div className='orv-list-upload'>
+                                  <div className='item'><i className='icon bi bi-image-fill'></i></div>
+                                  <div className='item'><p> ARQUIVO</p></div>
+                                  <div className='item' ><i className="bi bi-x-circle-fill"></i></div>
+                              </div>
+                              <div className='orv-list-upload'>
+                                  <div className='item'><i className='icon bi bi-image-fill'></i></div>
+                                  <div className='item'><p> ARQUIVO</p></div>
+                                  <div className='item' ><i className="bi bi-arrow-clockwise"></i></div>
                               </div>
                           </div>
                         </div>  
